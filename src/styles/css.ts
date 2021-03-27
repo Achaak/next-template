666907cae87753c406a728595fda9b59678bc74c
Achak <axel.lavoie@gmail.com>
2021-03-27 16:11:24 +0100

    🐛 Debug media css

--- src/styles/css.ts
+++ src/styles/css.ts
@@ -13,7 +13,7 @@ export const { styled, css, getCssString, global, theme } = createCss({
     sm: '(min-width: 640px)',
     md: '(min-width: 768px)',
     lg: '(min-width: 1024px)',
-    xl: '(min-width: 1024px)',
+    xl: '(min-width: 1280px)',
   },
   utils: {
     m: (_config) => (value) => ({
