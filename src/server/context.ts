f06085cff62b638123e9d71160ec335807253e83
Axel Lavoie <axel.lavoie@gmail.com>
2021-10-19 11:48:22 +0200

    fix prisma

--- src/server/context.ts
+++ src/server/context.ts
@@ -2,9 +2,25 @@ import { PrismaClient } from '@prisma/client'
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 
-export const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-})
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient
+    }
+  }
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: NodeJS.Global & { prisma?: PrismaClient }
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  })
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
+
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
