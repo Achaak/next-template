1976860255b4e1e3fc58e3a8b8394b7b0962a5bc
Axel Lavoie <axel.lavoie@gmail.com>
2021-10-21 15:58:56 +0200

    Add overmind tools

--- src/components/global/providerOvermind/ProviderOvermind.tsx
+++ src/components/global/providerOvermind/ProviderOvermind.tsx
@@ -3,7 +3,9 @@ import { createOvermind } from 'overmind'
 import { Provider } from 'overmind-react'
 import React from 'react'
 
-const overmind = createOvermind(config)
+const overmind = createOvermind(config, {
+  devtools: process.env.NODE_ENV === 'development',
+})
 
 export const ProviderOvermind: React.FC = ({ children }) => {
   return <Provider value={overmind}>{children}</Provider>
